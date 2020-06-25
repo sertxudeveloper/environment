4e4256330462a81f56cf7d39af8cff0c599f7b22
Sertxu Developer <dev.sertxu@gmail.com>
2020-06-25 16:41:47 +0200

    Update environment-init.sh

--- docker/environment-init.sh
+++ docker/environment-init.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y bind9
 
 # Download latest Docker
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
