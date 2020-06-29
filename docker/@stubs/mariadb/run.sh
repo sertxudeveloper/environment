3041e1ac191728bb4c94679e7f07320fe9775e8a
Sertxu Developer <dev.sertxu@gmail.com>
2020-06-29 17:11:18 +0200

    Update run.sh

--- docker/@stubs/mariadb/run.sh
+++ docker/@stubs/mariadb/run.sh
@@ -1,3 +1,3 @@
 docker run --name {{PROJECT_NAME}} \
-  -d --net docker-net --ip {{PROJECT_IP}} -dit --dns 10.0.0.30 \
+  -d --net docker-net --ip {{PROJECT_IP}} -dit --dns 172.18.0.1 \
   -p 3306:3306 -dit --restart unless-stopped --privileged mariadb:1.0
